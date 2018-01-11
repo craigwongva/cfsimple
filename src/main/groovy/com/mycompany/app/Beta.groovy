d6bc6c7133b46684210c5c3189e3257ba9d11583
EC2 Default User <ec2-user@ip-172-31-8-225.us-west-2.compute.internal>
2018-01-11 03:16:01 +0000

    Add curl

--- src/main/groovy/com/mycompany/app/Beta.groovy
+++ src/main/groovy/com/mycompany/app/Beta.groovy
@@ -10,6 +10,34 @@ public class Beta
     {
         System.out.println( "Hello World!" );
         println "A dog barks"
+        
+        def mickey = [
+          "curl",  
+          "${TEST_STACK_IP}:8080/green/timer/status"]
+        // .execute().text
+        def ARBITRARY_SUCCESS_PCT = 0.95
+        def NUM_GREEN_DOTS = 100
+        def GREEN_DOT_STATUS_DONE = '4'
+
+        //Groovy notes:
+        //fails: 
+        // if (mickey.indexOf(
+        //   GREEN_DOT_STATUS_DONE.multiply(
+        //     ARBITRARY_SUCCESS_PCT*NUM_GREEN_DOTS)) < 0) { }
+        //succeeds: 
+        // if (mickey.indexOf('4'.multiply(95)) < 0) { }
+
+        if (mickey.indexOf(
+          '4444444444444444444444444444444444444444444444444444444444') < 0) {
+    	  //error "red rover3 ${TEST_STACK_IP}:8080/green/timer/status $mickey" 
+        }
+        
+        def minnie = [
+            "curl",
+            "http://www.clarabridge.com"]
+            .execute().text
+        println minnie    
+        
         System.exit(0)
     }
 }
