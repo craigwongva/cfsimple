178d720c41c3ed8c12f28a7c375bec420acb82f9
EC2 Default User <ec2-user@ip-172-31-4-58.us-west-2.compute.internal>
2018-01-11 16:26:38 +0000

    Add S3 download code

--- src/main/groovy/com/mycompany/app/Beta.groovy
+++ src/main/groovy/com/mycompany/app/Beta.groovy
@@ -6,6 +6,14 @@ import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.amazonaws.services.s3.model.Bucket;
 import java.util.List;
 
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 /**
  * Hello world!
  *
@@ -45,6 +53,7 @@ public class Beta
         println minnie    
 
 String bucket_name = 'deleteme1001'
+/* succeeds:
 final AmazonS3 s3 = AmazonS3ClientBuilder.defaultClient();
 Bucket b = null;
 if (s3.doesBucketExist(bucket_name)) {
@@ -58,6 +67,30 @@ if (s3.doesBucketExist(bucket_name)) {
     }
 }
 //return b;
+*/
+String key_name = 'deleteme1002'
+final AmazonS3 s3 = AmazonS3ClientBuilder.defaultClient();
+try {
+    S3Object o = s3.getObject(bucket_name, key_name);
+    S3ObjectInputStream s3is = o.getObjectContent();
+    FileOutputStream fos = new FileOutputStream(new File(key_name));
+    byte[] read_buf = new byte[1024];
+    int read_len = 0;
+    while ((read_len = s3is.read(read_buf)) > 0) {
+        fos.write(read_buf, 0, read_len);
+    }
+    s3is.close();
+    fos.close();
+} catch (AmazonServiceException e) {
+    System.err.println(e.getErrorMessage());
+    System.exit(1);
+} catch (FileNotFoundException e) {
+    System.err.println(e.getMessage());
+    System.exit(1);
+} catch (IOException e) {
+    System.err.println(e.getMessage());
+    System.exit(1);
+}
         
         System.exit(0)
     }
