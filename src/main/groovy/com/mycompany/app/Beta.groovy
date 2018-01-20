715a1fc01ecb7ac98aa627ced6f0bb2546928af0
EC2 Default User <ec2-user@ip-172-31-6-54.us-west-2.compute.internal>
2018-01-20 11:16:28 +0000

    Introduce tegola

--- src/main/groovy/com/mycompany/app/Beta.groovy
+++ src/main/groovy/com/mycompany/app/Beta.groovy
@@ -4,32 +4,13 @@ public class Beta
 {
     public static void main( String[] args )
     {
-        //def mickey = [
-        //  "curl",  
-        //  "${TEST_STACK_IP}:8080/green/timer/status"]
-        // .execute().text
-        def ARBITRARY_SUCCESS_PCT = 0.95
-        def NUM_GREEN_DOTS = 100
-        def GREEN_DOT_STATUS_DONE = '4'
-
-        //Groovy notes:
-        //fails: 
-        // if (mickey.indexOf(
-        //   GREEN_DOT_STATUS_DONE.multiply(
-        //     ARBITRARY_SUCCESS_PCT*NUM_GREEN_DOTS)) < 0) { }
-        //succeeds: 
-        // if (mickey.indexOf('4'.multiply(95)) < 0) { }
-
-        //if (mickey.indexOf(
-        //  '4444444444444444444444444444444444444444444444444444444444') < 0) {
-    	  //error "red rover3 ${TEST_STACK_IP}:8080/green/timer/status $mickey" 
-        //}
 
         def test = [
           [ "http://saynext.redf4rth.net:8080/my-starter-app/prompt/font", /js>/ ],
           [ "http://geoserver.redf4rth.net/geoserver/web/", /org.geoserver.web.GeoServerBasePage/ ],
           [ "http://gocontainer.redf4rth.net:8080/green/timer/status",/4444444444/ ],
-          [ "http://www.clarabridge.com", /301 Moved Permanently/]
+          [ "http://tegola.redf4rth.net:8081/envelope2/envelope/sphere", /new ol.layer.VectorTile/],
+          [ "http://www.clarabridge.com", /301 Moved Permanently/],
         ]
 
         def iRecognize = []
